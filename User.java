09e5827578ce96f34b767582a91d399fefde95f6
Melanie McKune <melaniemck@outlook.com>
2021-05-11 10:09:48 -0700

    help me

--- User.java
+++ User.java
@@ -13,6 +13,25 @@ public class User {
     // accessor and modifier methods
 
 }
+  // Getter
+  public String getName() {
+    return name;
+  }
+
+  // Setter
+  public void setName(String newName) {
+    this.name = newName;
+  }
+  // Getter
+    public Fighter getFighter() {
+        return f;
+      }
+    
+      // Setter
+      public void setFighter(Fighter f) {
+        this.f = f;
+      }
+
 
 public createUser(){
     
