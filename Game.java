4121d9224daa8a85f111c4c89b68ef3ec15bb0f9
laasya <laasya_nagareddy@hotmail.com>
2021-05-12 10:52:21 -0700

    Game Methods + Test Commit-Process
    testing if the commiting files is working properly, also made a few modifications in the Game Class and added an empty text file for Fighter Data

--- Game.java
+++ Game.java
@@ -1,9 +1,11 @@
 import java.util.*;
 
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
 public class Game {
 
     // class variables
-    public Map<String, Double> characters;
+    public static Map<String, Double> characters;
 
     // accessor and modifier methods
 
@@ -13,6 +15,42 @@ public class Game {
     }
 
     public static void main(String[] args) {
+        setUpGame();
+        promptStartGame();
+    }
+
+    // setup game method
+    public static void setUpGame() {
+
+        // declare files
+
+        // get data from file to store in character array
+
+        // check data in map to ensure correctness
+
+    }
+
+    public static void promptStartGame() {
+
+        Scanner sc = new Scanner(System.in); // can change this later to be compatible with graphs
+        System.out.println("What is you name?");
+
+        String userName = sc.next();
+
+        System.out.println("Welome " + userName + "!");
+
+        // display character list
+        System.out.println("Which character would you like to choose?(Enter Number!)");
+
+        for (int i = 1; i <= characters.size(); i++) {
+            System.out.println("Character " + i + " : " + characters.get(i - 1));
+        }
+
+    }
+
+    // data for game methods
+
+    public void getDataFromFile(File file) {
 
     }
 
