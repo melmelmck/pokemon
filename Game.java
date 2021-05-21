31559433a173f69400756e79ce1c57bff216e6e3
laasya <laasya_nagareddy@hotmail.com>
2021-05-20 17:40:51 -0700

    Fighter Updates
    fixing a minor issue

--- Game.java
+++ Game.java
@@ -1,6 +1,5 @@
 import java.util.*;
-
-import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+import java.io.*;
 
 public class Game {
 
@@ -18,12 +17,57 @@ public class Game {
     }
 
     public static void main(String[] args) {
-        setUpGame();
-        promptStartGame();
+
+        getFighterData();
+        // promptStartGame();
     }
 
     // setup game method
+
+    public static void promptStartGame() {
+
+        Scanner sc = new Scanner(System.in); // can change this later to be compatible with graphs
+        System.out.println("What is you name?");
+
+        String userName = sc.next();
+
+        System.out.println("Welome " + userName + "!");
+
+        // display character list
+        System.out.println("Which character would you like to choose?(Enter Number!)");
+
+        for (int i = 1; i <= characters.size(); i++) {
+            System.out.println("Character " + i + " : " + characters.get(i - 1));
+        }
+
+    }
+
+    // data for game methods
+
+    public void getDataFromFiles(File file) {
+
+    }
+
     public static ArrayList<Fighter> getFighterData() {
+
+        Fighter tempFighter = null;
+        ArrayList<Fighter> characterArray = new ArrayList<Fighter>();
+
+        File fighterData = new File("pokemon/FighterData.txt");
+
+        try {
+            Scanner sc = new Scanner(fighterData);
+            // testing System.out.println(sc.nextLine());
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot find fighter data file.");
+        }
+
+        return characterArray;
+
+    }
+
+    public static ArrayList<Fighter> getSerialFighterData() {
 
         // deserialize
         Fighter tempFighter = null;
@@ -57,30 +101,6 @@ public class Game {
 
         // return data
         return characterArray;
-
-    }
-
-    public static void promptStartGame() {
-
-        Scanner sc = new Scanner(System.in); // can change this later to be compatible with graphs
-        System.out.println("What is you name?");
-
-        String userName = sc.next();
-
-        System.out.println("Welome " + userName + "!");
-
-        // display character list
-        System.out.println("Which character would you like to choose?(Enter Number!)");
-
-        for (int i = 1; i <= characters.size(); i++) {
-            System.out.println("Character " + i + " : " + characters.get(i - 1));
-        }
-
-    }
-
-    // data for game methods
-
-    public void getDataFromFile(File file) {
 
     }
 
