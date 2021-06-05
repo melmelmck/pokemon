5890e8736481b000d3a8a2647f24930cda9c7b60
laasya <laasya_nagareddy@hotmail.com>
2021-06-04 19:20:23 -0700

    slight changes??
    weird stuff

--- User.java
+++ User.java
@@ -2,19 +2,17 @@ import java.util.*;
 
 public class User {
 
-    // class variables
-    public String name;
-    private Fighter f;
-    private String fighterName;
+  // class variables
+  public String name;
+  private Fighter f;
+  private String fighterName;
 
-    // roung specific
-    private int roundsPlayed;
-    private Queue<Move> movesPlayed;
+  // roung specific
+  private int roundsPlayed;
+  private Queue<Move> movesPlayed;
 
-    // accessor and modifier methods
+  // accessor and modifier methods
 
-
-}
   // Getter
   public String getName() {
     return name;
@@ -24,6 +22,7 @@ public class User {
   public void setName(String newName) {
     this.name = newName;
   }
+
   // Getter
   public Fighter getFighter() {
     return f;
@@ -34,21 +33,22 @@ public class User {
     this.f = f;
   }
 
-public chooseFighter(){
-  // Game g = new Game(); //create an instance of Game class
-  // System.out.println("Here are your character choices: " + g.printCharacters()); //print list of fighters
-  System.out.println("Choose your fighter");
-  Scanner s = new Scanner(System.in);
-  while(s.hasNext){
-    fighterName = s.nextLine().toLowerCase(); 
-    f = new Fighter(fighterName); //create a new fighter with the name
+  public void chooseFighter() {
+    // Game g = new Game(); //create an instance of Game class
+    // System.out.println("Here are your character choices: " +
+    // g.printCharacters()); //print list of fighters
+    System.out.println("Choose your fighter");
+    Scanner s = new Scanner(System.in);
+    while (s.hasNext()) {
+      fighterName = s.nextLine().toLowerCase();
+      f = new Fighter(fighterName); // create a new fighter with the name
+    }
   }
-}
 
-public createUser(){
+public void createUser(){
   Scanner sc = new Scanner(System.in);
   System.out.println("User name: ");
-  while(sc.hasNext){
+  while(sc.hasNext()){
     name = sc.nextLine(); 
   }
 }
